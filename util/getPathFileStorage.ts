ad2e24f4b933e9bcc72d8755d005594c78e5d21d
Maicon <devmaicon@gmail.com>
2023-09-24 20:13:27 -0300

    update

--- util/getPathFileStorage.ts
+++ util/getPathFileStorage.ts
@@ -1,9 +1,5 @@
-import { SUPABASE_BUCKET } from "./constants";
+import { FOLDER_PROCESSING, FOLDER_RESTORED, STORAGE_URL, SUPABASE_BUCKET, SUPABASE_URL } from "./constants";
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
-const STORAGE_URL = process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL;
-const FOLDER_PROCESSING = process.env.NEXT_PUBLIC_FOLDER_PROCESSING
-const FOLDER_RESTORED = process.env.NEXT_PUBLIC_FOLDER_RESTORED
 
 type PathImage = "Processing" | "Restored"
 
@@ -14,8 +10,8 @@ type PathImageProps = {
     storageFullUrl?: boolean
 }
 
-// todo arquivo é gravado em uma pasta no bucket com id do usuário logado
-// essa função recebe o id do usuario logado, o path da imagem e o nome da imagem e gera a url da imagem.
+// todo arquivo é gravado em um bucket do supabase-storage em uma pasta com id do usuário logado
+// essa função recebe o id do usuario logado, o path da imagem e o nome da imagem e gera a url.
 // caso storageFullUrl seja informado, gera a url publica completa 
 
 export function getPathFileStorage({ userId, pathImagem, imageName, storageFullUrl }: PathImageProps) {
