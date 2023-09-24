d249801763d2fed0ab9de39888c3323a7ea4088b
Maicon <devmaicon@gmail.com>
2023-09-24 20:30:21 -0300

    update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         remotePatterns: [{ hostname: "qekizunytsypfkklwzte.supabase.co" }],
     },
+    experimental: {
+        serverActions: true,
+    },
 };
 
 module.exports = nextConfig;
